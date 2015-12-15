56c05ab05d796fda060117e9a6eebd428e484b60
Yaki Khadafi <ElSolDolLo@gmail.com>
2015-12-15 19:11:28 +0300

    0.9.13

--- WoTHack/Breakpoints.cs
+++ WoTHack/Breakpoints.cs
@@ -8,10 +8,10 @@ namespace WoTHack
     public class TreeRaidusBp : HardwareBreakPoint
     {
         // wg_setTreeHidingRadius
-        public static IntPtr Addr = new IntPtr(0x005350B8 - 0x400000);
-        public static IntPtr pSqrRad = new IntPtr(0x1B1671C - 0x400000);
-        public static IntPtr pMaxRad = new IntPtr(0x1B16720 - 0x400000);
-        public static IntPtr pMinRad = new IntPtr(0x1B16724 - 0x400000);
+        public static IntPtr Addr = new IntPtr(0x00536238 - 0x400000);
+        public static IntPtr pSqrRad = new IntPtr(0x1B134EC - 0x400000);
+        public static IntPtr pMaxRad = new IntPtr(0x1B134F0 - 0x400000);
+        public static IntPtr pMinRad = new IntPtr(0x1B134F4 - 0x400000);
 
         public static void WriteVals(float min, float max, MemoryHandler m)
         {
@@ -25,10 +25,10 @@ namespace WoTHack
         {
         }
 
-        // .text:005350B8                 movss   dword_1B1671C, xmm1
+        // .text:00536238 F3 0F 11 0D EC+                movss   dword_1B134EC, xmm1
         public override bool HandleException(ref CONTEXT ctx, ProcessDebugger pd)
         {
-            ctx.Eip += 0x8;
+            ctx.Eip += 8;
 
             WriteVals(1000, 1000, pd);
             return true;
@@ -37,9 +37,9 @@ namespace WoTHack
 
     public class AlwaysSniperBP : HardwareBreakPoint
     {
-        public static IntPtr Addr = new IntPtr(0x00533A4E - 0x400000);
+        public static IntPtr Addr = new IntPtr(0x00534BCE - 0x400000);
         // wg_enableTreeHiding
-        private static IntPtr enableHiding = new IntPtr(0x1B16718 - 0x400000);
+        private static IntPtr enableHiding = new IntPtr(0x1B134E8 - 0x400000);
 
         public static void WriteVals(bool enable, MemoryHandler m)
         {
@@ -51,10 +51,10 @@ namespace WoTHack
         {
         }
 
-        // .text:00533A4E                 mov     al, [ebp+var_1]
+        // .text:00534BCE 8A 45 FF                       mov     al, [ebp+var_1
         public override bool HandleException(ref CONTEXT ctx, ProcessDebugger pd)
         {
-            ctx.Eip += 0x51 - 0x4E;
+            ctx.Eip += 3;
             ctx.Al = 1;
             return true;
         }
